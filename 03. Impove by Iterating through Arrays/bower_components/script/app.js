bc5964623f6d541dfcc6d08754be82e1fb758989
gordonwang0718 <Gordon@MacBook-Pro.local>
2017-11-22 14:04:17 -0800

    - Building Blocks of Content

---
+++ 03. Impove by Iterating through Arrays/bower_components/script/app.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    function renderPage() {
+        var template = $('#index-template').html(),
+            compiled = Handlebars.compile(template),
+            rendered = compiled(window.language);
+
+        $('#main').html(rendered);
+        $('#languageSwitch').click(function() {
+          DogPack.switchLanguage();
+        });
+    }
+
+    function renderDogs() {
+        var template = $('#dogs-template').html(),
+            compiled = Handlebars.compile(template),
+            rendered = compiled({ dogs: DogPack.dogs, language: window.language });
+        $('#theDogs').html(rendered);
+    }
+    renderPage();
+    renderDogs();
+})();
