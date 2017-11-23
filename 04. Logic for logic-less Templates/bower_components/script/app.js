23aa917e0c35d834becfb4153454f51eb1c8231c
gordonwang0718 <Gordon@MacBook-Pro.local>
2017-11-22 18:24:38 -0800

    - Add Button Listener By using logic-less templates

---
+++ 04. Logic for logic-less Templates/bower_components/script/app.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+  function renderPage() {
+    var template = $('#index-template').html(),
+      compiled = Handlebars.compile(template),
+      rendered = compiled(window.language);
+
+    $('#main').html(rendered);
+    $('#languageSwitch').click(function() {
+      DogPack.switchLanguage();
+    });
+  }
+
+  function renderDogs() {
+    var template = $('#dogs-template').html(),
+      compiled = Handlebars.compile(template),
+      rendered = compiled({
+        dogs: DogPack.dogs,
+        language: window.language
+      });
+    $('#theDogs').html(rendered);
+    attachDogButtons();
+  }
+
+  function attachDogButtons() {
+    $('.dog-button').click(function() {
+      var id = $(this).closest('.dog-card').data('dog-id');
+      // The closest() method returns the first ancestor of the selected element.
+      // An ancestor is a parent, grandparent, great-grandparent, and so on.
+      DogPack.chooseDog(id);
+      renderDogs();
+    });
+
+    $('.not-dog-button').click(function() {
+      var id = $(this).closest('.dog-card').data('dog-id');
+      // The closest() method returns the first ancestor of the selected element.
+      // An ancestor is a parent, grandparent, great-grandparent, and so on.
+      DogPack.chooseNotDog(id);
+      renderDogs();
+    });
+
+  }
+  renderPage();
+  renderDogs();
+
+
+})();
