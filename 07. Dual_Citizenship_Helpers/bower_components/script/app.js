fe8adcf1c895d2e625f62c0830bfb9f27abec84c
gordonwang0718 <Gordon@MacBook-Pro.local>
2017-12-04 18:30:15 -0800

    - Dual_Citizenship_Helpers

---
+++ 07. Dual_Citizenship_Helpers/bower_components/script/app.js
@@ -0,0 +1,58 @@
+(function() {
+  'use strict';
+
+  registerPartials();
+  renderPage();
+  renderDogs();
+
+  function registerPartials() {
+    Handlebars.registerPartial('dog', $('#dog-template').html());
+  }
+
+  function renderPage() {
+    var template = $('#index-template').html(),
+      compiled = Handlebars.compile(template),
+      rendered = compiled(window.language);
+
+    $('#main').html(rendered);
+    $('#languageSwitch').click(function() {
+      DogPack.switchLanguage();
+    });
+
+    // add event listener on reset btn
+    $('#score').find('small').click(function() {
+      DogPack.clearDogs();
+      window.location.href = '?' + Handlebars.helpers.getLanguageFilter(window.language.langId);
+    });
+  }
+
+  function renderDogs() {
+    var template = $('#dogs-template').html(),
+      compiled = Handlebars.compile(template),
+      rendered = compiled({
+        dogs: DogPack.dogs,
+        language: window.language
+      });
+    $('#theDogs').html(rendered);
+    attachDogButtons();
+  }
+
+  function attachDogButtons() {
+    $('.dog-button').click(function() {
+      var id = $(this).closest('.dog-card').data('dog-id');
+      // The closest() method returns the first ancestor of the selected element.
+      // An ancestor is a parent, grandparent, great-grandparent, and so on.
+      DogPack.chooseDog(id);
+      renderDogs();
+    });
+
+    $('.not-dog-button').click(function() {
+      var id = $(this).closest('.dog-card').data('dog-id');
+      // The closest() method returns the first ancestor of the selected element.
+      // An ancestor is a parent, grandparent, great-grandparent, and so on.
+      DogPack.chooseNotDog(id);
+      renderDogs();
+    });
+
+  }
+})();
